9d4a49b2fc36e4722ad30f4c009677a64f77161f
Jnanesh <jnaneshps4@gmail.com>
2024-01-09 08:13:59 +0530

    Add files via upload

---
+++ removeDuplicatesinLL.cpp
@@ -0,0 +1,66 @@
+#include<iomanip>
+
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    
+    Node(int d)
+    {
+        this->data=d;
+        this->next=NULL;
+    }
+    
+    ~Node()
+    {
+        int value=this->data;
+        
+        while(this->next!=NULL)
+        {
+            delete next;
+            this=this->next;
+        }
+        cout<<"memory is free"<<endl;
+    }
+    
+};
+
+Node* uniqusortedList(Node* head)
+{
+    //empty list
+    if(head==NULL)
+    return NULL;
+    
+    //non-empty list
+    
+    Node* curr=head;
+    
+    while(curr!=NULL)
+    {
+        
+        if((curr->next!=NULL) && curr->data==curr->next->data)
+        {
+            Node* next_node=curr->next->next;
+            Node* nodeTodelete=curr->next;
+            delete(nodeTodelete);
+            curr->next=next_node;
+           
+            
+        }
+        else{
+            curr=curr->next;
+        }
+    }
+}
+
+int main()
+{
+    
+    
+    
+    
+    
+    return 0;
+}
