87dd500e6cfbdb25572c62bc5c7b38323643b40b
Jnanesh <jnaneshps4@gmail.com>
2024-01-10 07:29:52 +0530

    Add files via upload

---
+++ checkPlaindromeinLL.cpp
@@ -0,0 +1,67 @@
+//srt 0,1,2 in the linked list
+
+#include <iostream>
+#include<vector>
+
+using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    Node* next;
+    
+    Node(int d)
+    {
+        this->data=d;
+        this->next=NULL;
+        }
+    ~Node()
+    {
+        int value=this->data;
+        while(this->next!=NULL)
+        {
+            delete next;
+            this->next=NULL;
+        }
+        cout<<"memory is freed and and memory is freed"<<endl;
+    }
+};
+
+bool checkPalindrome(vector<int> arr)
+{
+    int n=arr.size();
+    int s=0;
+    int e=n-1;
+    
+    while(s<=e)
+    {
+        if(arr[s]!=arr[e])
+        {
+            return 0;
+        }
+        s++;
+        e--;
+    }
+    return 1;
+}
+
+bool ispalindrome(Node* head)
+{
+    vector<int> arr;
+    Node* temp=head;
+    while(temp!=NULL)
+    {
+        arr.push_back(temp->data);
+        temp=temp->data;
+    }
+    return checkPalindrome(arr); 
+}
+
+int main()
+{
+   
+
+    return 0;
+}
+
