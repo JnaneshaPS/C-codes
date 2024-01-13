5c540e3b16457f8c2b269cc9ab5afc71aea63087
Jnanesh <jnaneshps4@gmail.com>
2024-01-13 07:58:26 +0530

    Add files via upload

---
+++ reversethestack.cpp
@@ -0,0 +1,33 @@
+//reversing the string using the stack
+#include<iostream>
+#include<string>
+#include<stack>
+
+using namespace std;
+
+int main()
+{
+    string str="vaishak";
+    
+    stack <char> s;
+    
+    for(int i=0;i<str.length();i++)
+    {
+        char ch=str[i];
+        s.push(ch);
+    }
+   
+    string ans="";
+    
+    while(!s.empty())
+    {
+        char ch=s.top();
+        ans.push_back(ch);
+        
+      s.pop();
+    }
+    cout<<"string is"<< " "<<ans<<endl;
+    
+    return 0;
+    
+}
