aadb86bb1150ed75461a237569486b8edb33ceff
Jnanesh <jnaneshps4@gmail.com>
2024-05-17 06:49:36 +0530

    Create search-in-rotated-array.cpp

---
+++ leet-code/search-in-rotated-array.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int search(std::vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            // If left half is sorted
+            if (nums[left] <= nums[mid]) {
+                // If target is within the sorted half
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else { // If right half is sorted
+                // If target is within the sorted half
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1; // Target not found
+    }
+};
