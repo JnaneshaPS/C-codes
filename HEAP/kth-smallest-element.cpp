6bbc3f5dbb26d38220e3c7c6b285b3ad3e5d4453
Jnanesh <jnaneshps4@gmail.com>
2024-06-04 07:13:29 +0530

    Update kth-smallest-element.cpp

--- HEAP/kth-smallest-element.cpp
+++ HEAP/kth-smallest-element.cpp
@@ -1,1 +1,25 @@
+class Solution
+//arr :given array
+//l : first index of the array
+//r : size-1 index of the given array
+//k:smallest element to find in the array
+{
+public:
+       int kthsmallest(int arr[],int l,int r,int k)
+{
+  priority_queue<int> pq;
 
+  for(int i=0;i<k;i++)
+{
+  pq.push(arr[i]);
+}
+for(int i=k;i<=r;i++)
+{
+if(arr[i]<pq.top())
+{
+  pq.pop();
+pq.push(arr[i]);
+}
+}
+int ans=pq.top();
+return ans;
