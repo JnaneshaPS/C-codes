f23389d9afd87bf8ccd16660448e8f17c2a6a4e3
Jnanesh <jnaneshps4@gmail.com>
2024-06-30 11:41:08 +0530

    Update topological-sorting-by-kahns'(bfs).cpp

--- GRAPH/topological-sorting-by-kahns'(bfs).cpp
+++ GRAPH/topological-sorting-by-kahns'(bfs).cpp
@@ -1,1 +1,55 @@
 
+
+vector<int> topologicalSort(int e,int v,vector<vector<int>> &edges)
+{
+     unordered_map<int.list<int>> &adj;
+     for(int i=0;i,e;i++)
+       {
+            int u=edges[i][0];
+            int v=edges[i][1];
+
+
+  adj[u].push_back(v);
+     }
+vector<int> indegree(V);
+//find all the indegree
+   for(auto i:adj)
+{
+    for(auto j:i.second)
+{   
+      indegree[j]++;
+}
+}
+//push the 0 degree elements
+     queue<int> q;
+      for(int i=0;i<v;i++)
+{
+         if(indegree[i]==0)
+{
+        q.push(i);
+}
+}
+
+ //do bfs
+vector<int> ans;
+   while(!q.empty())
+{
+     int front=q.front();
+       q.pop();
+
+
+   //ans store
+     ans.push_back(front);
+
+  //neighbour indegree update
+    for(auto neighbour:adj[front])
+      { 
+          indegree[neighbour]--;
+         if(indegree[neighbour]==0)
+           q.push(neighbour);
+      }
+}
+ return ans;
+}
+    
+
