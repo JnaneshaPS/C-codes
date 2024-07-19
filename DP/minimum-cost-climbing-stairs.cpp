8ce4004686550d38d0fc667e0755e319dbf76ede
Jnanesh <jnaneshps4@gmail.com>
2024-07-19 08:35:59 +0530

    Create minimum-cost-climbing-stairs.cpp

---
+++ DP/minimum-cost-climbing-stairs.cpp
@@ -0,0 +1,21 @@
+int solve2(vector<int> &cost,int n,vector<int> &dp)
+{
+    if(n==0)
+      return cost[0];
+    if(n==1)
+      return cost[1];
+
+    if(dp[n]!=-1)
+       return dp[n];
+
+    dp[n]=cost[n]+min(solve2(cost,n-1,dp),solve2(cost,n-2,dp));
+    return dp[n];       
+}
+
+    int minCostClimbingStairs(vector<int>& cost) {
+        
+        int n=cost.size();
+        vector<int> dp(n+1,-1);
+        int ans=min(solve2(cost,n-1,dp),solve2(cost,n-2,dp));
+        return ans;
+    }
