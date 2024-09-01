2386fe23ccbbd14ab48b55a4701af46ba1bb6d35
Jnanesh <jnaneshps4@gmail.com>
2024-09-01 18:17:58 +0530

    Create Missing-in-Array.cpp

---
+++ ARRAY/Missing-in-Array.cpp
@@ -0,0 +1,18 @@
+class solution{
+//given the size of the array we need to find the missing number in the array
+
+int missingArray(vector<int> &arr,int n)
+{
+    int sun=(n*n(+1)/2);
+
+int expectedSum=0;
+
+   for(int i=0;i<arr.size();i++)
+{
+       exprectedSum=expectedSum+arr[i];
+}
+
+int ans=sum-expectedSu;
+return ans;
+}
+};
