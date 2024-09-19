b51cc1793b8eed6088ea47afbf26d0f7892778b1
Jnanesh <jnaneshps4@gmail.com>
2024-09-19 21:37:11 +0530

    Update Set-Matrix-Zeroes.cpp

--- ARRAY/Set-Matrix-Zeroes.cpp
+++ ARRAY/Set-Matrix-Zeroes.cpp
@@ -26,23 +26,41 @@ for(int i=0;i<n;i++)
 
 }
 
+    for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+                {
+                    if(matrix[i][j]==0)
+                    {
+                         matrix[i][0]=0;
+                        matrix[0][j]=0;
+                    }
+                }
+        }
+
+    
+
 for(int i=1;i<m;i++)
 {
-      matrix[i][0]=0;
+     if(matrix[i][0]=0)
+     {
        for(int j=1;j<n;j++)
 {
      matrix[i][j]=0;
 }
+     }
 }
 
 
 for(int i=1;i<n;i++)
-{   
-           matrix[0][i]=0;
+{  
+    if(matrix[0][i]=0)
+    {
         for(int j=1;j<m;j++)
 {     
          matrix[i][j]=0;
 }
+    }
 }
 
 if(firstRow)
