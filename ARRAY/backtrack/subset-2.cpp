aa946ed57e3935857dd8d743ae113d0ecb01b768
Jnanesh <jnaneshps4@gmail.com>
2024-09-24 17:52:27 +0530

    Create subset-2.cpp

---
+++ ARRAY/backtrack/subset-2.cpp
@@ -0,0 +1,22 @@
+//Given an integer array nums that may contain duplicates, return all possible subsets (the power set).
+//The solution set must not contain duplicate subsets. Return the solution in any order.
+
+
+void backtrack(vector<int> &nums,vector<vector<int>> &result,vector<int> &combination,int start)
+{
+                result.push_back(combination);
+               for(int i=start;i<nums.size();i++)
+{
+                combination.push_back(nums[i]);
+           backtrack(nums,result,combination,i+1);
+              combination.pop_back();
+}
+}
+
+vector<vector<int>> subset(vector<int> &nums)
+{
+         vector<vector<int>> result;
+             vector<int> combination;
+             backtrack(nums,result,combination,0);
+          return result;
+}
