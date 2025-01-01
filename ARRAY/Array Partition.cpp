c17c110d79d57392d286df97692054e36bc41270
Jnanesh <jnaneshps4@gmail.com>
2025-01-01 07:55:42 +0530

    Create Array Partition.cpp

---
+++ ARRAY/Array Partition.cpp
@@ -0,0 +1,22 @@
+//Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2), ..., (an, bn) such that the sum of min(ai, bi) for all i is maximized. Return the maximized sum.
+//Input: nums = [1,4,3,2]
+//Output: 4
+//Explanation: All possible pairings (ignoring the ordering of elements) are:
+//1. (1, 4), (2, 3) -> min(1, 4) + min(2, 3) = 1 + 2 = 3
+//2. (1, 3), (2, 4) -> min(1, 3) + min(2, 4) = 1 + 2 = 3
+//3. (1, 2), (3, 4) -> min(1, 2) + min(3, 4) = 1 + 3 = 4
+//So the maximum possible sum is 4.
+
+int ArrayPartition(vector<int> &nums)
+{
+   sort(nums.begin(),nums.end());
+
+  int ans=0;
+for(int i=0;i<nums.size();i+=2)
+{
+   ans+=nums[i];
+}
+
+return ans;
+}
+      
